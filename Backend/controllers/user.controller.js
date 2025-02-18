707384e3c3a58be262618ed49fa8ccb59b32aaba
Vedansh Gupta <vedansh83138@gmail.com>
2025-02-18 21:55:47 +0530

    minor updates

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,1 +1,7 @@
 const userModel = require('../models/user.model');
+const userService = require('../services/user.service');
+
+
+module.exports.registerUser = async (req, res, next) => {
+
+}
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -7,6 +7,8 @@ router.post('/register' [
     body('email').isEmail().withMessage('Invalid Email'),
     body('fullname.firstname').isLength({ min: 3 }).withMessage('First name must be at least 3 character long'),
     body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 character long')
-])
+],
+    userController.registerUser
+)
 
 module.exports = router;
